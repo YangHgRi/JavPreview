0cfa81fd8f7cc1133a13a1a18b3501bb931e4a85
YangHgRi <1987758983@qq.com>
2022-12-16 01:03:13 +0800

    1. url拼接流程优化与代码质量改良

--- src/main/java/com/yanghgri/processor/MainProcess.java
+++ src/main/java/com/yanghgri/processor/MainProcess.java
@@ -2,60 +2,49 @@ package com.yanghgri.processor;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.text.NumberFormat;
+
 /**
  * @author YangHgRi
  */
 public class MainProcess {
-    public static final int LIMITED_LENGTH = 3;
+    private static final int LIMITED_LENGTH = 3;
+    private static final NumberFormat NUMBER_FORMATTER = NumberFormat.getInstance();
+    private static final String URL_PREFIX = "https://cc3001.dmm.co.jp/litevideo/freepv/";
+
+    static {
+        NUMBER_FORMATTER.setGroupingUsed(false);
+        NUMBER_FORMATTER.setMinimumIntegerDigits(5);
+        NUMBER_FORMATTER.setMaximumIntegerDigits(5);
+    }
 
     private MainProcess() {
     }
 
     public static String[] process(String[] avIds) {
-        // https://cc3001.dmm.co.jp/litevideo/freepv/a/abc/abc00123/abc00123_dmb_w.mp4
+        // https://cc3001.dmm.co.jp/litevideo/freepv/a/abc/abc00123/abc00123_mhb_w.mp4
         String[] urls = new String[avIds.length];
         for (int i = 0; i < avIds.length; i++) {
             String[] lettersAndNumbers = StringUtils.split(avIds[i], "-", 0);
             String letters = lettersAndNumbers[0];
-            String numbers = lettersAndNumbers[1];
-            urls[i] = getUrl(letters, numbers);
+            String numbersWithZeroFill = NUMBER_FORMATTER.format(Long.valueOf(lettersAndNumbers[1]));
+
+            urls[i] = getUrl(letters, numbersWithZeroFill);
         }
         return urls;
     }
 
     private static String getUrl(String letters, String numbers) {
+        StringBuilder urlBuilder = new StringBuilder();
+        urlBuilder.append(URL_PREFIX).append(letters.charAt(0)).append("/");
+
         if (letters.length() < LIMITED_LENGTH) {
-            return "https://cc3001.dmm.co.jp/litevideo/freepv/"
-                    + letters.charAt(0)
-                    + "/"
-                    + letters
-                    + "/"
-                    + letters
-                    + "00"
-                    + numbers
-                    + "/"
-                    + letters
-                    + "00"
-                    + numbers
-                    + "_dmb_w.mp4";
+            urlBuilder.append(letters);
         } else {
-            return "https://cc3001.dmm.co.jp/litevideo/freepv/"
-                    + letters.charAt(0)
-                    + "/"
-                    + letters.substring(0, 3)
-                    + "/"
-                    + letters
-                    /*版本更新，不需要这两个 00 了*/
-                    /*+ "00"*/
-                    + numbers
-                    + "/"
-                    + letters
-                    /*+ "00"*/
-                    + numbers
-                    /*+ "_dmb_w.mp4";*/
-                    /*mhb 画质高于 dmb*/
-                    + "_mhb_w.mp4";
-            // https://cc3001.dmm.co.jp/litevideo/freepv/g/gvh/gvh482/gvh482_mhb_w.mp4
+            urlBuilder.append(letters, 0, 3);
         }
+
+        urlBuilder.append("/").append(letters).append(numbers).append("/").append(letters).append(numbers).append("_mhb_w.mp4");
+        return urlBuilder.toString();
     }
 }
